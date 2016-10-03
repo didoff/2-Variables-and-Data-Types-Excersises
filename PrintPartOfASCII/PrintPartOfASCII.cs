10d107894dd078042aa20eb3daeefc18e294bcea
didoff <dlucky95@gmail.com>
2016-10-03 21:05:37 +0300

    Completed Print of ASCII

---
+++ PrintPartOfASCII/PrintPartOfASCII.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PrintPartOfASCII
+{
+    class PrintPartOfASCII
+    {
+        static void Main(string[] args)
+        {
+            //char startSymbol = char.Parse(Console.ReadLine());
+            //char endSymbol = char.Parse(Console.ReadLine());
+
+            int startSymbol = int.Parse(Console.ReadLine());
+            int endSymbol = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < endSymbol - startSymbol +1; i++)
+            {
+
+                char n = Convert.ToChar(startSymbol + i);
+                Console.Write(n + " ");
+            }
+
+            Console.WriteLine();
+            
+        }
+    }
+}
